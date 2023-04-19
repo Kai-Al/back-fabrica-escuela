f0c21187e83b2637e092e9131308246d4a851f3a
Jose Waldo <josferw@gmail.com>
2023-04-19 10:57:22 -0500

    init: cree el proyecto base para trabajar con springboot

---
+++ plangestionestudios/src/main/java/com/plangestionestudios/gestion/PlangestionestudiosApplication.java
@@ -0,0 +1,13 @@
+package com.plangestionestudios.gestion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlangestionestudiosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlangestionestudiosApplication.class, args);
+	}
+
+}
