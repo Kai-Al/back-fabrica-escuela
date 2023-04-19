b1753e494d7591d51131819120537153a63f46c7
Jose Waldo <josferw@gmail.com>
2023-04-19 11:21:45 -0500

    feat: agregue la entidad de unidad academica

--- plangestionestudios/src/main/java/com/plangestionestudios/gestion/entities/AcademicUnit.java
+++ plangestionestudios/src/main/java/com/plangestionestudios/gestion/entities/AcademicUnit.java
@@ -1,2 +1,25 @@
-package com.plangestionestudios.gestion.entities;public class AcademicUnit {
+package com.plangestionestudios.gestion.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "unidad_academica")
+public class AcademicUnit {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int idAcademinUnit;
+    @Column(name = "codigo_unidad_academica")
+    private String codeAcademicUnit;
+    @Column(name = "tipo_unidad_academica")
+    private String typeAcademicUnit;
+    @Column(name = "nombre_decano")
+    private String deanName;
+    @Column(name = "descripcion_unidad_academica")
+    private String description;
 }
