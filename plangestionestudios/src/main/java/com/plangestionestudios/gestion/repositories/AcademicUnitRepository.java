0b6f35455f45ea2f14c64db98c5f711359aaf00f
Jose Waldo <josferw@gmail.com>
2023-04-19 11:23:58 -0500

    feat: agregue el repositorio de unidad academica

---
+++ plangestionestudios/src/main/java/com/plangestionestudios/gestion/repositories/AcademicUnitRepository.java
@@ -0,0 +1,7 @@
+package com.plangestionestudios.gestion.repositories;
+
+import com.plangestionestudios.gestion.entities.AcademicUnit;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AcademicUnitRepository extends JpaRepository<AcademicUnit, Integer> {
+}
