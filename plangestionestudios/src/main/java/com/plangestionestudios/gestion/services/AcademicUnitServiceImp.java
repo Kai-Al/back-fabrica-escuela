382eac6c85ce0d77b9f90cb2475e1b30bc8c3e08
Jose Waldo <josferw@gmail.com>
2023-04-19 17:15:41 -0500

    feat: añadi la firma del metodo para buscar por uno y su implementación

--- plangestionestudios/src/main/java/com/plangestionestudios/gestion/services/AcademicUnitServiceImp.java
+++ plangestionestudios/src/main/java/com/plangestionestudios/gestion/services/AcademicUnitServiceImp.java
@@ -6,15 +6,25 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class AcademicUnitServiceImp implements AcademicUnitService{
 
     @Autowired
     private AcademicUnitRepository academicUnitRepository;
+
     @Override
     public List<AcademicUnit> getAllAcademicUnits() {
         List<AcademicUnit> academicUnitList = academicUnitRepository.findAll();
         return academicUnitList;
     }
+
+    @Override
+    public AcademicUnit getAcademicUnitById(int id) {
+        Optional<AcademicUnit> academicUnit = academicUnitRepository.findById(id);
+        if(!academicUnit.isPresent()) return null;
+        return academicUnit.get();
+    }
+
 }
