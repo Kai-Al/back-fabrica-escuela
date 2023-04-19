b4f9a4566485cafc281dc8de103853b3d1f4eca0
Jose Waldo <josferw@gmail.com>
2023-04-19 17:16:33 -0500

    feat: cree un nuevo endpoint para encontrar por id una unidad academica

--- plangestionestudios/src/main/java/com/plangestionestudios/gestion/controllers/AcademicUnitController.java
+++ plangestionestudios/src/main/java/com/plangestionestudios/gestion/controllers/AcademicUnitController.java
@@ -4,11 +4,9 @@ import com.plangestionestudios.gestion.entities.AcademicUnit;
 import com.plangestionestudios.gestion.services.AcademicUnitService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -20,10 +18,18 @@ public class AcademicUnitController {
     private AcademicUnitService academicUnitService;
 
     @GetMapping("/all")
-    @ResponseStatus(HttpStatus.OK)
-    public List<AcademicUnit> getAllAcademicUnits() {
+    public ResponseEntity<List<AcademicUnit>> getAllAcademicUnits() {
         List<AcademicUnit> academicUnitList = academicUnitService.getAllAcademicUnits();
-        return academicUnitList;
+        return new ResponseEntity<>(academicUnitList, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<AcademicUnit> getAcademicUnitById(@PathVariable("id") int id) {
+        AcademicUnit academicUnit = academicUnitService.getAcademicUnitById(id);
+        return new ResponseEntity<>(academicUnit, HttpStatus.OK);
+    }
+    public void createAcademicUnits() {
+
     }
 
 }
