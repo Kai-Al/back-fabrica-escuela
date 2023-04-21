7744d70a28049d6c48b9246647e6cec119d4de8c
Jose Waldo <josferw@gmail.com>
2023-04-21 10:05:53 -0500

    feat: agregue la interfaz de los servicios de subunidades academicas y agregue los metodos a la implementacion

--- plangestionestudios/src/main/java/com/plangestionestudios/gestion/controllers/AcademicSubUnitController.java
+++ plangestionestudios/src/main/java/com/plangestionestudios/gestion/controllers/AcademicSubUnitController.java
@@ -1,2 +1,15 @@
-package com.plangestionestudios.gestion.controllers;public class AcademicSubUnitController {
+package com.plangestionestudios.gestion.controllers;
+
+import com.plangestionestudios.gestion.services.AcademicSubUnitService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("api/academicSubUnit")
+@RestController
+public class AcademicSubUnitController {
+
+    @Autowired
+    private AcademicSubUnitService academicSubUnitService;
+
 }
