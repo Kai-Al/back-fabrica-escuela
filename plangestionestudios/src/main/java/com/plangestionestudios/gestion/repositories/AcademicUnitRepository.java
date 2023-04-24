256ac487ee23446b690a5d9420859b4372293b55
Jose Waldo <josferw@gmail.com>
2023-04-24 11:07:23 -0600

    feat: agregue un nuevo metodo findBy para buscar por codigo una unidad academica

--- plangestionestudios/src/main/java/com/plangestionestudios/gestion/repositories/AcademicUnitRepository.java
+++ plangestionestudios/src/main/java/com/plangestionestudios/gestion/repositories/AcademicUnitRepository.java
@@ -3,5 +3,8 @@ package com.plangestionestudios.gestion.repositories;
 import com.plangestionestudios.gestion.entities.AcademicUnit;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface AcademicUnitRepository extends JpaRepository<AcademicUnit, Integer> {
+    List<AcademicUnit> findByCodeAcademicUnit(String codeAcademicUnit);
 }
