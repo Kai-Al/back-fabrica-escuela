a35f26e2670f1d63ea9824ce54447863e63d1697
Jose Waldo <josferw@gmail.com>
2023-04-25 18:14:18 -0600

    feat: añadí el controlador para traer todas las SUA's

--- plangestionestudios/src/main/java/com/plangestionestudios/gestion/controllers/AcademicSubUnitController.java
+++ plangestionestudios/src/main/java/com/plangestionestudios/gestion/controllers/AcademicSubUnitController.java
@@ -1,9 +1,16 @@
 package com.plangestionestudios.gestion.controllers;
 
+import com.plangestionestudios.gestion.entities.AcademicSubUnit;
+import com.plangestionestudios.gestion.entities.AcademicUnit;
 import com.plangestionestudios.gestion.services.AcademicSubUnitService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RequestMapping("api/academicSubUnit")
 @RestController
@@ -11,5 +18,11 @@ public class AcademicSubUnitController {
 
     @Autowired
     private AcademicSubUnitService academicSubUnitService;
+
+    @GetMapping("/all")
+    public ResponseEntity<List<AcademicSubUnit>> getAllAcademicUnits() {
+        List<AcademicSubUnit> academicSubUnitList = this.academicSubUnitService.getAllAcademicSubUnits();
+        return new ResponseEntity<>(academicSubUnitList, HttpStatus.OK);
+    }
 
 }
