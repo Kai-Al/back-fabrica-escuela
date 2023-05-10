8ec208a1364125b95a0bce3ae4483affe9bafbb8
Kai-Al <Kai.albornoz@udea.edu.co>
2023-05-10 13:44:16 -0500

    Feat: Test unitarios sub unidades academicas.

---
+++ plangestionestudios/src/test/java/com/plangestionestudios/gestion/AcademicSubUnitControllerTest.java
@@ -0,0 +1,38 @@
+package com.plangestionestudios.gestion;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import com.plangestionestudios.gestion.entities.AcademicSubUnit;
+import com.plangestionestudios.gestion.controllers.AcademicSubUnitController;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class AcademicSubUnitControllerTest {
+    @Autowired
+    private AcademicSubUnitController academicSubUnitController;
+    static AcademicSubUnit subAcademicUnit = new AcademicSubUnit();
+
+    @BeforeAll
+    public static void setUp() {
+        subAcademicUnit.setCodeAcademicSubUnit("code");
+        subAcademicUnit.setTypeAcademicSubUnit("type");
+        subAcademicUnit.setNameAcademicSubUnit("name");
+        subAcademicUnit.setHeadName("head");
+        subAcademicUnit.setDescription("description");
+    }
+
+    @Test
+    void testGetAllAcademicUnits() {
+        assertEquals(HttpStatus.OK, academicSubUnitController.getAllAcademicSubUnits().getStatusCode());
+    }
+
+    @Test
+    void testCreateAcademicUnits() {
+        ResponseEntity<AcademicSubUnit> response = academicSubUnitController.createAcademicSubUnits(subAcademicUnit);
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+    }
+
+}
