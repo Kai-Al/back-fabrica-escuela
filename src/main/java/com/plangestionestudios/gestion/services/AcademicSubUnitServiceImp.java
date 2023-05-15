915f50c126784f7b82759becba50853edb071ac1
Jose Waldo <josferw@gmail.com>
2023-05-15 14:32:02 -0500

    feat: añadi un metodo privado para evaluar los campos obligatorios y además añadi validaciones al servicio

--- src/main/java/com/plangestionestudios/gestion/services/AcademicSubUnitServiceImp.java
+++ src/main/java/com/plangestionestudios/gestion/services/AcademicSubUnitServiceImp.java
@@ -6,6 +6,7 @@ import com.plangestionestudios.gestion.repositories.AcademicSubUnitRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Arrays;
 import java.util.List;
 
 @Service
@@ -34,11 +35,46 @@ public class AcademicSubUnitServiceImp implements AcademicSubUnitService{
 
     @Override
     public AcademicSubUnit createAcademicSubUnit(AcademicSubUnit academicSubUnit) {
-        int idAcademicUnit = academicSubUnit.getUnidadAcademica().getIdAcademicUnit();
+
+        if(!this.isCorrectInfoASU(academicSubUnit)) return null;
+
+        int idAcademicUnit = academicSubUnit.getAcademicUnit().getIdAcademicUnit();
         AcademicUnit academicUnit = this.academicUnitService.getAcademicUnitById(idAcademicUnit);
+
+        System.out.println(academicUnit);
+
         if(academicUnit == null) return null;
-        academicSubUnit.setUnidadAcademica(academicUnit);
-        this.academicSubUnitRepository.save(academicSubUnit);
-        return academicSubUnit;
+
+        String codeAcademicSubUnit = academicSubUnit.getCodeAcademicSubUnit();
+        List<AcademicSubUnit> academicSubUnitFound = academicSubUnitRepository.findByCodeAcademicSubUnit(codeAcademicSubUnit);
+        System.out.println(academicSubUnitFound);
+        if(academicSubUnitFound.isEmpty()) {
+            academicSubUnit.setAcademicUnit(academicUnit);
+            this.academicSubUnitRepository.save(academicSubUnit);
+            return academicSubUnit;
+        }
+        return null;
+    }
+
+    private boolean isCorrectInfoASU(AcademicSubUnit academicSubUnit) {
+
+        if(academicSubUnit == null) return false;
+
+        String codeASU = academicSubUnit.getCodeAcademicSubUnit();
+        String nameASU = academicSubUnit.getNameAcademicSubUnit();
+        String typeASU = academicSubUnit.getTypeAcademicSubUnit();
+        String headASU = academicSubUnit.getHeadName();
+        String descASU = academicSubUnit.getDescription();
+        AcademicUnit academicUnitASU = academicSubUnit.getAcademicUnit();
+
+        String[] typesASU = {"departamento", "instituto", "escuela"};
+
+        if(codeASU == null || nameASU == null || typeASU == null || descASU == null || headASU == null || academicUnitASU == null) return false;
+        if(codeASU.length() == 0) return false;
+
+        boolean isFoundTypeASU = Arrays.stream(typesASU).anyMatch(type -> type.equals(typeASU.toLowerCase()));
+        if(!isFoundTypeASU) return false;
+
+        return true;
     }
 }
