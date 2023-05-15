2c673e26fa9bb3d9648c1902177245074157056e
Jose Waldo <josferw@gmail.com>
2023-05-15 14:33:45 -0500

    controlador listo

--- src/main/java/com/plangestionestudios/gestion/controllers/AcademicSubUnitController.java
+++ src/main/java/com/plangestionestudios/gestion/controllers/AcademicSubUnitController.java
@@ -26,8 +26,9 @@ public class AcademicSubUnitController {
     @PostMapping("")
     public ResponseEntity<AcademicSubUnit> createAcademicSubUnits(@RequestBody AcademicSubUnit academicSubUnit) {
         AcademicSubUnit academicSubUnitCreated = this.academicSubUnitService.createAcademicSubUnit(academicSubUnit);
+        System.out.println("Sub unidad que llega del servicio: " + academicSubUnitCreated);
         if(academicSubUnitCreated != null) return new ResponseEntity<>(academicSubUnitCreated,HttpStatus.OK);
-        return new ResponseEntity<>(null,HttpStatus.OK);
+        return new ResponseEntity<>(null,HttpStatus.BAD_REQUEST);
     }
 
 }
