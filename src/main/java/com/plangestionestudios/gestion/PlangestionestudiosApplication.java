00a898b4e78411b8f20c8f0fc6c5884770886a88
Jose Waldo <josferw@gmail.com>
2023-05-20 07:23:43 -0500

    feat: añadí más direcciones validas a la configuración del cors

--- src/main/java/com/plangestionestudios/gestion/PlangestionestudiosApplication.java
+++ src/main/java/com/plangestionestudios/gestion/PlangestionestudiosApplication.java
@@ -19,7 +19,9 @@ public class PlangestionestudiosApplication {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
 				registry.addMapping("/**")
-						.allowedOrigins("http://127.0.0.1:5500", "http://localhost:5500", "https://pla-udea-front.vercel.app")
+						.allowedOrigins("http://127.0.0.1:5500", "http://localhost:5500",
+								"http://127.0.0.1:3000", "http://localhost:3000",
+								"https://pla-udea-front.vercel.app")
 						.allowedMethods("*")
 						.allowedHeaders("*");
 			}
