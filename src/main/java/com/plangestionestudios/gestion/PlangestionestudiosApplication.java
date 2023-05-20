820ca8ce0ec01342bca7ab02f92b5c4ce80eea3f
Jose Waldo <josferw@gmail.com>
2023-05-19 21:19:15 -0500

    fix: arreglando el problema del cors

--- src/main/java/com/plangestionestudios/gestion/PlangestionestudiosApplication.java
+++ src/main/java/com/plangestionestudios/gestion/PlangestionestudiosApplication.java
@@ -18,7 +18,10 @@ public class PlangestionestudiosApplication {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/api/**").allowedOrigins("http://localhost:3000", "http://127.0.0.1:5500", "https://pla-udea-front.vercel.app").allowedMethods("**");
+				registry.addMapping("/**")
+						.allowedOrigins("http://127.0.0.1:5500", "http://localhost:5500", "https://pla-udea-front.vercel.app")
+						.allowedMethods("*")
+						.allowedHeaders("*");
 			}
 		};
 	}
